45ca869d0c3ab795885fd5642c1ec0f10b99d86e
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2022-12-25 03:37:45 +0900

    12.25 flutter design:box, text..etc

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('앱제목'),
+      appBar: AppBar(
+        centerTitle: false,
+        backgroundColor: Colors.white,
+        title: Row(
+          children: [
+            Text('금호동3가',
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                    color: Colors.black)),
+            Icon(
+              Icons.expand_more,
+              color: Colors.black,
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+            color: Colors.black,
           ),
-          body: Center(
-            child: Image.asset('assets/flutter.png'),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu),
+            color: Colors.black,
           ),
-          bottomNavigationBar: BottomAppBar(
-            child: SizedBox(
-              height: 70,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.phone),
-                  Icon(Icons.message),
-                  Icon(Icons.contact_page),
-                ],
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notifications),
+            color: Colors.black,
+          ),
+        ],
+      ),
+      body: Container(
+        height: 150,
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Image.asset(
+              'assets/flutter.png',
+              width: 150,
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('캐논 DSLR 100D(단렌즈, \n충전기 16기가SD 포함)',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text('성동구 행당동 · 끌올 10분 전',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 12,
+                      ),
+                    ),
+                    Text('210,000원',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite_border),
+                        Text('4'),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        )
-    );
+          ],
+        ),
+      ),
+    ));
   }
 }

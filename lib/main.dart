c8cf60d1d2a9b721af6fe0a2a6aa817d2d056767
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2023-01-02 19:37:07 +0900

    1.2 Listview.builder and floatingActionButton, layout of flutter is over

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 // stless
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  var a = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: Text('코딩애플 연락처 앱'),
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()), // 재렌더링이 안되어서 그렇다 표기가 안됩니다.
+          onPressed: () {
+            a++;
+            print(a);
+          },
+        ),
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('코딩애플 연락처 앱'),
+        ),
+        body: ListView.builder(
+          itemCount: 300,
+          itemBuilder: (context, i) {
+            return ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('오스모스'),
+            );
+          },
+        ),
+        bottomNavigationBar: CustomBottomBar(),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('홍길동'),
-          ),
-          ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('홍길동'),
-          ),
-          ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('홍길동'),
-          ),
-        ],
-      ),
-      bottomNavigationBar: CustomBottomBar(),
-    ));
+    );
   }
 }
 

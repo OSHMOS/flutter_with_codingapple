a44e2a709b9df478f35edb783ac711ce34d19d69
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2023-01-02 20:11:22 +0900

    1.2 state and setState for rerendering in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,44 @@ void main() {
   runApp(MyApp());
 }
 
-// stless
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({super.key});
 
-  var a = 1;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var name = ['엄마', '아빠', '형'];
+  var like = [0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Text(a.toString()), // 재렌더링이 안되어서 그렇다 표기가 안됩니다.
-          onPressed: () {
-            a++;
-            print(a);
-          },
-        ),
         appBar: AppBar(
           centerTitle: false,
           title: Text('코딩애플 연락처 앱'),
         ),
         body: ListView.builder(
-          itemCount: 300,
+          itemCount: 3,
           itemBuilder: (context, i) {
             return ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('오스모스'),
+              leading: Text(like[i].toString()),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(name[i]),
+                  ElevatedButton(
+                    child: Text('좋아요'),
+                    onPressed: () {
+                      setState(() {
+                        like[i]++;
+                      });
+                    },
+                  ),
+                ],
+              ),
             );
           },
         ),
